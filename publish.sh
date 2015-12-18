66659ec4be7aac88e05b67d5cbe19e8ae3f8cc19
nxq90 <arsenal.nxq@gmail.com>
2015-12-18 13:35:34 +0700

    test is part of publish

--- publish.sh
+++ publish.sh
@@ -4,13 +4,24 @@ packages=("utils")
 currDir=$(pwd)
 version=$@
 
+echo "Running package tests"
+
+for i in "${packages[@]}"
+do :
+   echo "Switch to ${currDir}/src/${i}"
+   cd "${currDir}/src/${i}"
+   echo "Running [npm run test]"
+   npm run test
+   echo "Done."
+done
+
 echo "Bumping all packages version"
 
 for i in "${packages[@]}"
 do :
    echo "Switch to ${currDir}/src/${i}"
    cd "${currDir}/src/${i}"
-   echo "Running npm version ${version}"
+   echo "Running [npm version ${version}]"
    npm version ${version}
    echo "Done."
 done
@@ -23,7 +34,7 @@ for i in "${packages[@]}"
 do :
    echo "Switch to ${currDir}/src/${i}"
    cd "${currDir}/src/${i}"
-   echo "Running npm publish"
+   echo "Running [npm publish]"
    npm publish
    echo "Done."
 done
